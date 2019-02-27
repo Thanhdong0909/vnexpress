f4a15e3b1ef94d6ae72b6c9345f254eb543034f7
Phan Thanh Dong <phanthanhdongtd@gmail.com>
2019-02-27 23:52:08 +0700

    add information file hosting

--- model/model_db.php
+++ model/model_db.php
@@ -4,6 +4,9 @@ class dataBase{
 	private static $dsn='mysql:host=localhost;dbname=db_vnexpress';
 	private static $username = 'root';
 	private static $password = '';
+	// private static $dsn='mysql:host=sql108.byethost14.com;dbname=b14_23520896_ptd';
+	//  private static $username = 'b14_23520896';
+	//  private static $password = 'dangthuytram261142';
 	private static $db;
 	private static $option = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");
 
